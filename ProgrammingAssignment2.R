64d927c57af43c5514b4962c2f9cb997ffa150a5
garunbe <garunbe@gmail.com>
2020-05-28 17:05:23 +0530

    garunbe_Programming assignment

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,30 @@
+#the aim of these functions is to calculate an inversed matrix.
+makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+##This function will compute what was obtained by the previous one
+
+cacheSolve <- function(x, ...) {
+  
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
